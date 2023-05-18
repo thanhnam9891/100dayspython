04c89e45e195c697ac6b15b6981e7e497dfff8d9
nam nguyen <namnguyen@noname.com>
2023-05-18 08:49:15 +0700

    day60 commit

--- day60-challenge.py
+++ day60-challenge.py
@@ -1,1 +1,22 @@
-#
+import datetime
+
+print("🌟Event Countdown Timer🌟")
+
+event= input("Event Name > ")
+eventday = int(input("Day: ")) # Get all input as numbers. We're not at text input for months yet.
+eventmonth = int(input("Month: "))
+eventyear = int(input("Year: "))
+eventdate = datetime.date(eventyear, eventmonth, eventday)
+today = datetime.date.today()
+
+difference = eventdate-today
+difference = difference.days
+
+if difference == 0:
+    message = "Today is your birthday. Happy birthday to you."
+elif difference < 0:
+    message = "Hope you enjoyed your bithday"
+else:
+    message = f"Your birthday is {difference} day(s) left."
+
+print(message)
--- day60-learn.py
+++ day60-learn.py
@@ -18,3 +18,18 @@ year = int(input("Year: "))
 date = datetime.date(year, month, day)
 
 print(date)
+
+difference = datetime.timedelta(days=14) # The difference I want
+
+newDate = today + difference # Add today to the delta difference to see the date in 14 days time.
+
+print(newDate)
+
+holiday = datetime.date(year = 2022, month = 10, day = 30) # The date of my holiday
+
+if holiday > today: # If my holiday is in the future
+  print("Coming soon")
+elif holiday < today: #If my holiday date has passed
+  print("Hope you enjoyed it")
+else: # If my holiday date is today
+  print("HOLIDAY TIME!")
